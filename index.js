41400a5555733026b10bf18e2903898d3017464b
mjkim <mel917@naver.com>
2021-05-02 18:16:51 +0900

    html,css 1차완성

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ const menu = document.querySelector('.nav');
 toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
+
+
+/**
+ * 순서대로 나타나는 기능
+ */
+// 나타날 요소들(.fade-in) 찾기.
+const fadeEls = document.querySelectorAll('.fade-in')
+// 나타날 요소들을 하나씩 반복해서 처리!
+fadeEls.forEach(function (fadeEl, index) {
+  // 각 요소들을 순서대로(delay) 보여지게 함!
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  })
+})
